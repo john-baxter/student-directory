59abe6ac646fa40c10594f2b8dcc110bd7e94b93
John Baxter <john.g.baxter@googlemail.com>
2020-02-11 20:29:39 +0000

    more refactoring - added methods

--- directory.rb
+++ directory.rb
@@ -16,10 +16,30 @@ students = [
 
 # and then print them
 
-puts "The students of Villains Academy"
-puts "-------------"
-students.each |student| do
-  puts student
+# puts "The students of Villains Academy"
+# puts "-------------"
+# students.each |student| do
+#   puts student
+# end
+# # then we say how many students we have - without adding line breaks
+# puts "Overall we have #{students.count} great students"
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
 end
-# then we say how many students we have - without adding line breaks
-puts "Overall we have #{students.count} great students"
+
+def print_array(names)
+  names.each do |name|
+    puts name
+  end
+end
+
+def print_footer(names)
+  puts "\nOverall we have #{names.count} great students"
+end
+
+# call the methods
+print_header
+print_array(students)
+print_footer(students)
