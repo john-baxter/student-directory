047b3d130387f98cf17748d5fcf0ed9fc88b7675
John Baxter <john.g.baxter@googlemail.com>
2020-02-12 11:29:36 +0000

    add updates for some of the exercised (step 8)

--- directory.rb
+++ directory.rb
@@ -1,29 +1,33 @@
-
-# # put all students in an array
-# students = [
-#   {name: "Dr. Hannibal Lecter",          cohort: :november},
-#   {name: "Darth Vader",                  cohort: :november},
-#   {name: "Nurse Ratched",                cohort: :november},
-#   {name: "Michael Corleone",             cohort: :november},
-#   {name: "Alex DeLarge",                 cohort: :november},
-#   {name: "The Wicked Witch of the West", cohort: :november},
-#   {name: "Terminator",                   cohort: :november},
-#   {name: "Freddy Krueger",               cohort: :november},
-#   {name: "The Joker",                    cohort: :november},
-#   {name: "Joffrey Baratheon",            cohort: :november},
-#   {name: "Norman Bates",                 cohort: :november}
-# ]
-
-def get_students # assume November cohort (for now) as per instructions
+def get_students # assume November cohort (right now) as per instructions
   puts "Please enter the name of a student: "
   puts "To finish press 'enter' twice."
 
+  possible_cohorts = [
+    :January,
+    :February,
+    :March,
+    :April,
+    :May,
+    :June,
+    :July,
+    :August,
+    :September,
+    :October,
+    :Novemeber,
+    :December
+  ]
+  
   students = []
   student = gets.chomp
 
+
   while !student.empty? do 
     students << {name: student, cohort: :November}
-    puts "Now we have #{students.count} students"
+      if students.count == 1
+        puts "Now we have #{students.count} student"
+      else
+        puts "Now we have #{students.count} students"
+      end
     student = gets.chomp
   end
 
@@ -36,8 +40,10 @@ def print_header
 end
 
 def print_array(people)
-  people.each do |person|
-    puts "#{person[:name]} (#{person[:cohort]} cohort)"
+  people_printed = 0
+  while people_printed < people.count do
+    puts "#{people_printed + 1}. #{people[people_printed][:name]} (#{people[people_printed][:cohort]} cohort)"
+    people_printed += 1
   end
 end
 
@@ -46,8 +52,30 @@ def print_footer(names)
   puts ""
 end
 
+def print_specific_initial(people, initial)
+  puts "Those whos name begins with #{initial}"
+  puts ""
+  people.each.with_index(1) do |person, idx|
+    if person[:name].chr == initial
+      puts "#{idx}. #{person[:name]} (#{person[:cohort]} cohort)"
+    end    
+  end
+end
+
+def print_twelve_minus(people)
+  puts "Those with names shorter than 12 characters"
+  puts ""
+  people.each.with_index(1) do |person, idx|
+    if person[:name].length < 12
+      puts "#{idx}. #{person[:name]} (#{person[:cohort]} cohort)"
+    end    
+  end
+end
+
 # call the methods
 students = get_students
 print_header
+# print_specific_initial(students, "t")
+# print_twelve_minus(students)
 print_array(students)
 print_footer(students)
